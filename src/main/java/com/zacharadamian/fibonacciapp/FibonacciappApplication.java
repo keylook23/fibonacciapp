97265bc86311a68b2b22db2c8faf669e963175fa
keylook23 <zachara_damian@wp.pl>
2018-06-24 13:46:51 +0200

    Version 1

--- src/main/java/com/zacharadamian/fibonacciapp/FibonacciappApplication.java
+++ src/main/java/com/zacharadamian/fibonacciapp/FibonacciappApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FibonacciappApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(FibonacciappApplication.class, args);
     }
-
 }
